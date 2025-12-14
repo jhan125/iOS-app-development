8c14757ab58f680110a3121e25e3c45386fffab8
Jiali <98071264+jhan125@users.noreply.github.com>
2025-12-14 15:40:56 -0800

    Trip Map App

---
+++ TripMap/TripMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripMap
+//
+//  Created by Jiali Han on 11/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TripMap/TripMapTests/TripMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  TripMapTests.swift
+//  TripMapTests
+//
+//  Created by Jiali Han on 11/22/24.
+//
+
+import Testing
+@testable import TripMap
+
+struct TripMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
